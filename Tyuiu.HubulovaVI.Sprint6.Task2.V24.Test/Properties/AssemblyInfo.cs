1815b02e3ecf86a33d826d6523fd56f2165249c2
HubulovaVI <valeriahb@mail.ru>
2023-12-03 17:04:13 +0500

    Task2

---
+++ Tyuiu.HubulovaVI.Sprint6.Task2.V24.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.HubulovaVI.Sprint6.Task2.V24.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.HubulovaVI.Sprint6.Task2.V24.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b68f519-f4cb-4b5f-82ec-07988604df03")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
